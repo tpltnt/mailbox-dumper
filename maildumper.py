f98259e53754c0418ca31979d51746cdf5b9f438
tpltnt <tpltnt.github@dropcut.net>
2013-12-13 01:16:58 +0100

    more graceful socket error handling

--- maildumper.py
+++ maildumper.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-import getpass, poplib, sys
+import getpass, poplib, socket, sys
 
 if 2 != len(sys.argv):
     print("usage: " + str(sys.argv[0]) + " hostname")
     sys.exit(1)
 
 # set account parameter & connect
-account = poplib.POP3_SSL(sys.argv[1])
+try:
+    account = poplib.POP3_SSL(sys.argv[1])
+except socket.gaierror:
+    print("can not connect to '" + str(sys.argv[1]) + "' on the default SSL port")
+    sys.exit(2)
+
 account.user(getpass.getuser())
 account.pass_(getpass.getpass())
 # iterate over all messages
